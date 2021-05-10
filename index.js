53e71e5d10500281f495c142d2efef2e9ca35905
DJDora <matthewheas@gmail.com>
2021-05-10 10:51:36 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const config = require('./config.json')
+
+client.on('message', message => {
+    if (message.content === '!whale') {
+        number = 11;
+        imageNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
+        message.channel.send ( {files: ["./images/whale" + imageNumber + ".png"]})
+    } 
+})g
+
+client.on('message', message => {
+    if (message.content === '!dolphin') {
+        number = 11;
+        imageNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
+        message.channel.send ( {files: ["./images/dolphin" + imageNumber + ".png"]})
+    } 
+})
+
+client.on('message', message => {
+    if (message.content === '!sealion') {
+        number = 11;
+        imageNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
+        message.channel.send ( {files: ["./images/sealion" + imageNumber + ".png"]})
+    } 
+})
+
+
+client.on('ready', () => {
+    console.log('Bot ready!')
+})
+
+client.login(config.token)
